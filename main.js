e160502dd9c331b450e4647c1fd88a833b714acd
夢 ɯɐǝɹp <907209562@qq.com>
2019-05-28 00:37:25 +1000

    fixed new

--- main.js
+++ main.js
@@ -2,19 +2,21 @@ module.exports.loop = function(){
     var creep_list = [];
     var creep = Game.creeps['Proj1279_Harvester1', 'Proj1279_Harvester2'];
 
-    if (creep.carry.energy < creep.carryCapacity){
+    for (var i = 0; i < creep.length; i++){
+        if (creep[i].carry.energy < creep[i].carryCapacity){
 
-        var souces = creep.room.find(FIND_SOURCES);
-        
-        if (creep.harvest(souces[0]) == ERR_NOT_IN_RANGE){
-            creep.moveTo(souces[0]);
-        }
-        else {
-            if (creep.transfer(Game.
-                spawns['Project1279_Substance1'], RESOURCE_ENERGY)==
-                ERR_NOT_IN_RANGE){
-                    creep.moveTo(Game.spawns['Project1279_Substance1']);
-                }
+            var souces = creep.room.find(FIND_SOURCES);
+            
+            if (creep[i].harvest(souces[0]) == ERR_NOT_IN_RANGE){
+                creep[i].moveTo(souces[0]);
+            }
+            else {
+                if (creep[i].transfer(Game.
+                    spawns['Project1279_Substance1'], RESOURCE_ENERGY)==
+                    ERR_NOT_IN_RANGE){
+                        creep[i].moveTo(Game.spawns['Project1279_Substance1']);
+                    }
+            }
         }
     }
 }
