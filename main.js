eb66d56771697ffd720385dbd9b9e600c291c3fa
夢 ɯɐǝɹp <907209562@qq.com>
2019-05-28 00:44:53 +1000

    two harverster

--- main.js
+++ main.js
@@ -1,21 +1,19 @@
 module.exports.loop = function(){
-    var creep_list = [];
-    var creep = Game.creeps['Proj1279_Harvester1', 'Proj1279_Harvester2'];
 
-    for (var i = 0; i < creep.length; i++){
-        if (creep[i].carry.energy < creep[i].carryCapacity){
+    for (var name in Game.creeps){
 
+        var creep = Game.creeps[name];
+
+        if (creep.carry.energy < creep.carryCapacity){
             var souces = creep.room.find(FIND_SOURCES);
-            
-            if (creep[i].harvest(souces[0]) == ERR_NOT_IN_RANGE){
-                creep[i].moveTo(souces[0]);
+            if (creep.harvest(souces[0]) == ERR_NOT_IN_RANGE){
+                creep.moveTo(souces[0]);
             }
-            else {
-                if (creep[i].transfer(Game.
-                    spawns['Project1279_Substance1'], RESOURCE_ENERGY)==
+        }
+        else {
+            if (creep.transfer(Game.spawns['Project1279_Substance1'], RESOURCE_ENERGY)==
                     ERR_NOT_IN_RANGE){
-                        creep[i].moveTo(Game.spawns['Project1279_Substance1']);
-                    }
+                    creep.moveTo(Game.spawns['Project1279_Substance1']);
             }
         }
     }
