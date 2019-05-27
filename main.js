127a22dff008cb16f9898ea50e69eb189ee18bb9
夢 ɯɐǝɹp <907209562@qq.com>
2019-05-28 00:09:51 +1000

    still constructing

---
+++ main.js
@@ -0,0 +1,10 @@
+module.exports.loop = function(){
+    var creep = Game.creeps['Proj1279_Harvester1'];
+
+    if (creep.carry.energy < creep.carryCapacity){
+
+        var souces = creep.room.find(FIND_SOURCES);
+        
+        if 
+    }
+}
