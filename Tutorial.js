3934bf16ab70968749bacb8789afd5f74a77ec3e
夢 ɯɐǝɹp <907209562@qq.com>
2019-05-28 11:36:41 +1000

    tutorial 1 finished

--- Tutorial.js
+++ Tutorial.js
@@ -1,3 +1,85 @@
-建立农民:
+建立挖矿机:
 Game.spawns['Spawn1'].spawnCreep( [WORK, CARRY, MOVE], 'Harvester1' );
 
+采集资源：
+module.exports.loop = function () {
+    var creep = Game.creeps['Harvester1'];
+    var sources = creep.room.find(FIND_SOURCES);
+    if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(sources[0]);
+    }
+}
+
+采集并上缴资源：
+module.exports.loop = function () {
+    var creep = Game.creeps['Harvester1'];
+
+    if(creep.carry.energy < creep.carryCapacity) {
+        var sources = creep.room.find(FIND_SOURCES);
+        if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(sources[0]);
+        }
+    }
+    else {
+        if( creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE ) {
+            creep.moveTo(Game.spawns['Spawn1']);
+        }
+    }
+}
+
+建立第二个挖矿机：
+Game.spawns['Spawn1'].spawnCreep( [WORK, CARRY, MOVE], 'Harvester2' );
+
+两个挖矿机关联执行同样任务：
+module.exports.loop = function () {
+    for(var name in Game.creeps) {
+        var creep = Game.creeps[name];
+
+        if(creep.carry.energy < creep.carryCapacity) {
+            var sources = creep.room.find(FIND_SOURCES);
+            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0]);
+            }
+        }
+        else {
+            if(creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(Game.spawns['Spawn1']);
+            }
+        }
+    }
+}
+
+
+分开module：
+Main:
+
+    var roleHarvester = require('role.Harvester');
+
+    module.exports.loop = function () {
+        for(var name in Game.creeps) {
+            var creep = Game.creeps[name];
+            roleHarvester.run(creep);
+        }
+    }
+
+role.harvester:
+
+    var roleHarvester = {
+        /** @param {Creep} creep **/
+
+        run: function(creep) {
+            var creep = Game.creeps['Harvester1'];
+            if(creep.carry.energy < creep.carryCapacity) {
+                var sources = creep.room.find(FIND_SOURCES);
+                if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(sources[0]);
+                }
+            }
+            else {
+                if(creep.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(Game.spawns['Spawn1']);
+                }
+            }
+        }
+    };
+    module.exports = roleHarvester;
--- main.js
+++ main.js
@@ -1,20 +1,19 @@
-module.exports.loop = function(){
+    module.exports.loop = function(){
 
-    for (var name in Game.creeps){
+        for (var name in Game.creeps){
 
-        var creep = Game.creeps[name];
+            var creep = Game.creeps[name];
 
-        if (creep.carry.energy < creep.carryCapacity){
-            var souces = creep.room.find(FIND_SOURCES);
-            if (creep.harvest(souces[0]) == ERR_NOT_IN_RANGE){
-                creep.moveTo(souces[0]);
+            if (creep.carry.energy < creep.carryCapacity){
+                var souces = creep.room.find(FIND_SOURCES);
+                if (creep.harvest(souces[0]) == ERR_NOT_IN_RANGE){
+                    creep.moveTo(souces[0]);
+                }
             }
-        }
-        else {
-            if (creep.transfer(Game.spawns['Project1279_Substance1'], RESOURCE_ENERGY)==
-                    ERR_NOT_IN_RANGE){
-                    creep.moveTo(Game.spawns['Project1279_Substance1']);
+            else {
+                if (creep.transfer(Game.spawns['Project1279_Substance1'], RESOURCE_ENERGY)== ERR_NOT_IN_RANGE){
+                        creep.moveTo(Game.spawns['Project1279_Substance1']);
+                }
             }
         }
     }
-}
