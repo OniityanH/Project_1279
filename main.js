e270e8a0286d1921811bddfeb942aa094c76f0b3
夢 ɯɐǝɹp <907209562@qq.com>
2019-05-29 20:58:09 +1000

    loop for each harvester

--- main.js
+++ main.js
@@ -1,23 +1,27 @@
     module.exports.loop = function(){
-        var creep = Game.creeps.Harvester1;
 
-        if (creep.memory.working == true && creep.carry.energy == 0){
-            creep.memory.working = false;
-        }
-        else if(creep.memory.working == false && creep.carry.energy == creep.carryCapacity){
-            creep.memory.working = true;
-        }
+        for (let name in Game.creeps){
+            
+            var creep = Game.creeps[name];
 
-        if (creep.memory.working == true){
-            if (creep.transfer(Game.spawns.Spswn1, 
-                RESOURCE_ENERGY) == ERR_NOT_IN_RANGE){
-                    creep.moveTo(Game.Spswn1);
+            if (creep.memory.working == true && creep.carry.energy == 0){
+                creep.memory.working = false;
+            }
+            else if(creep.memory.working == false && creep.carry.energy == creep.carryCapacity){
+                creep.memory.working = true;
+            }
+
+            if (creep.memory.working == true){
+                if (creep.transfer(Game.spawns.Spswn1, 
+                    RESOURCE_ENERGY) == ERR_NOT_IN_RANGE){
+                        creep.moveTo(Game.Spswn1);
+                    }
+            }
+            else{
+                var source = creep.pos.findClosestByPath(FIND_SOURCES);
+                if (creep.harvest(source) == ERR_NOT_IN_RANGE){
+                    creep.moveTo(source);
                 }
-        }
-        else{
-            var source = creep.pos.findClosestByPath(FIND_SOURCES);
-            if (creep.harvest(source) == ERR_NOT_IN_RANGE){
-                creep.moveTo(source);
             }
         }
     }
