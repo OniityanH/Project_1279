848ae4de4af2d0cf86ffc1d9934584cb5d79adbd
夢 ɯɐǝɹp <907209562@qq.com>
2019-05-30 00:02:52 +1000

    tutorial 4

--- main.js
+++ main.js
@@ -1,29 +1,56 @@
-    module.exports.loop = function(){
+// import modules
+var roleHarvester = require('role.harvester');
+var roleUpgrader = require('role.upgrader');
 
-        for (let name in Game.creeps){
-            
-            var creep = Game.creeps[name];
-
-            console.log(name + " is working: " + creep.memory.working);
-
-            if (creep.memory.working == true && creep.carry.energy == 0){
-                creep.memory.working = false;
-            }
-            else if(creep.memory.working == false && creep.carry.energy == creep.carryCapacity){
-                creep.memory.working = true;
-            }
-
-            if (creep.memory.working == true){
-                if (creep.transfer(Game.spawns.Spswn1, 
-                    RESOURCE_ENERGY) == ERR_NOT_IN_RANGE){
-                        creep.moveTo(Game.Spswn1);
-                    }
-            }
-            else{
-                var source = creep.pos.findClosestByPath(FIND_SOURCES);
-                if (creep.harvest(source) == ERR_NOT_IN_RANGE){
-                    creep.moveTo(source);
-                }
-            }
+module.exports.loop = function () {
+    // check for memory entries of died creeps by iterating over Memory.creeps
+    for (let name in Memory.creeps) {
+        // and checking if the creep is still alive
+        if (Game.creeps[name] == undefined) {
+            // if not, delete the memory entry
+            delete Memory.creeps[name];
         }
     }
+
+    // for every creep name in Game.creeps
+    for (let name in Game.creeps) {
+        // get the creep object
+        var creep = Game.creeps[name];
+
+        // if creep is harvester, call harvester script
+        if (creep.memory.role == 'harvester') {
+            roleHarvester.run(creep);
+        }
+        // if creep is upgrader, call upgrader script
+        else if (creep.memory.role == 'upgrader') {
+            roleUpgrader.run(creep);
+        }
+    }
+
+    // goal: have 10 harvesters and as many upgraders as possible
+    var minimumNumberOfHarvesters = 10;
+    // _.sum will count the number of properties in Game.creeps filtered by the
+    //  arrow function, which checks for the creep being a harvester
+    var numberOfHarvesters = _.sum(Game.creeps, (c) => c.memory.role == 'harvester');
+    var name = undefined;
+
+    // if not enough harvesters
+    if (numberOfHarvesters < minimumNumberOfHarvesters) {
+        // try to spawn one
+        name = Game.spawns.Spawn1.createCreep([WORK,WORK,CARRY,MOVE], undefined,
+            { role: 'harvester', working: false});
+    }
+    else {
+        // else try to spawn an upgrader
+        // small change from what you saw in the video: for upgraders it makes
+        //  more sense to have two move parts because they have to travel further
+        name = Game.spawns.Spawn1.createCreep([WORK,CARRY,MOVE,MOVE], undefined,
+            { role: 'upgrader', working: false});
+    }
+
+    // print name to console if spawning was a success
+    // name > 0 would not work since string > 0 returns false
+    if (!(name < 0)) {
+        console.log("Spawned new creep: " + name);
+    }
+};
