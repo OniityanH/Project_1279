376c463b0d6b12ed00bb9a28709e1dd7fc5cc79a
夢 ɯɐǝɹp <907209562@qq.com>
2019-06-01 20:19:17 +1000

    create builder tutorial 5

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
 var HarvesterNumber = 0;
 var UpgraderNumber = 0;
+var BuilderNumber = 0;
 
 module.exports.loop = function () {
     // check for memory entries of died creeps by iterating over Memory.creeps
@@ -31,9 +32,13 @@ module.exports.loop = function () {
 
     // goal: have 10 harvesters and as many upgraders as possible
     var minimumNumberOfHarvesters = 10;
+    var minimumNumberOfUpgraders = 1;
+    var minimumNumberOfBuilders = 1;
     // _.sum will count the number of properties in Game.creeps filtered by the
     //  arrow function, which checks for the creep being a harvester
     var numberOfHarvesters = _.sum(Game.creeps, (c) => c.memory.role == 'harvester');
+    var numberOfUpgraders = _.sum(Game.creeps, (c) => c.memory.role == 'upgrader');
+    var numberOfBuilders = _.sum(Game.creeps, (c) => c.memory.role == 'builder');
     var name = undefined;
 
     // if not enough harvesters
@@ -43,13 +48,24 @@ module.exports.loop = function () {
             { role: 'harvester', working: false});
         HarvesterNumber = HarvesterNumber + 1;
     }
+    else if (numberOfUpgraders < minimumNumberOfUpgraders){
+        name = Game.spawns.Spawn1.createCreep([WORK,CARRY,MOVE,MOVE], 'Upgrader_' + UpgraderNumber,
+            { role: 'upgrader', working: false});
+        UpgraderNumber = UpgraderNumber + 1;
+    }
+
+    else if (numberOfBuilders < minimumNumberOfBuilders){
+        name = Game.spawns.Spawn1.createCreep([WORK,WORK,CARRY,MOVE], 'Builder_' + BuilderNumber,
+            { role: 'builder', working: false});
+        BuilderNumber = BuilderNumber + 1;
+    }
     else {
         // else try to spawn an upgrader
         // small change from what you saw in the video: for upgraders it makes
         //  more sense to have two move parts because they have to travel further
-        name = Game.spawns.Spawn1.createCreep([WORK,CARRY,MOVE,MOVE], 'Upgrader_' + UpgraderNumber,
-            { role: 'upgrader', working: false});
-        UpgraderNumber = UpgraderNumber + 1;
+        name = Game.spawns.Spawn1.createCreep([WORK,WORK,CARRY,MOVE], 'Builder_' + BuilderNumber,
+            { role: 'builder', working: false});
+        BuilderNumber = BuilderNumber + 1;
     }
 
     // print name to console if spawning was a success
